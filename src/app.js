5d41fc436efbcc66de0ffe1efe7c7019e7260a3f
Alexander <aradostev@softaria.com>
2023-04-29 17:56:27 +0900

    logging to telegram

--- src/app.js
+++ src/app.js
@@ -1,6 +1,20 @@
 import { Application } from "./application.js";
+import { TelegramBotWrapper } from "./test.js";
 
 const app = new Application();
+const bot = new TelegramBotWrapper();
+
+const originalConsoleLog = console.log;
+const originalConsoleError = console.error;
+
+console.log = (message) => {
+  bot.sendLog(message);
+  originalConsoleLog(message);
+};
+console.error = (message) => {
+  bot.sendError(message);
+  originalConsoleError(message);
+};
 // const cron = require("./resetCounterCronJob");
 
 // const testInterval = require("./testLimiterCronJob");
--- src/test.js
+++ src/test.js
@@ -1,5 +1,33 @@
-import { moderateMessage } from "./utils/openaiUtils.js";
+import TelegramBot from "node-telegram-bot-api";
+import dotenv from "dotenv";
+dotenv.config();
+const token = process.env.TG_BOT_TOKEN;
 
-moderateMessage("child violence is unforgivable").then((r) => {
-  console.log(r);
-});
+export class TelegramBotWrapper {
+  constructor() {
+    if (token) {
+      this.bot = new TelegramBot(token, { polling: true });
+      this.bot.on("channel_post", (msg) => {
+        const chatId = msg.chat.id;
+
+        // send a message to the chat acknowledging receipt of their message
+        this.bot.sendMessage(chatId, "Received your message");
+      });
+    }
+    this.logsId = -1001906303858;
+    this.errorsId = -1001906303858;
+  }
+
+  sendLog(message) {
+    if (this.bot) {
+      this.bot.sendMessage(this.logsId, message);
+    }
+  }
+  sendError(message) {
+    if (this.bot) {
+      this.bot.sendMessage(this.errorsId, message);
+    }
+  }
+}
+
+// Matches "/echo [whatever]"
