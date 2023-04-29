ccf3c2f8389a540a7a408cff98174cfaa90d5811
Alexander <aradostev@softaria.com>
2023-04-29 22:23:06 +0900

    trying out object logging

--- src/app.js
+++ src/app.js
@@ -7,18 +7,35 @@ import {
 
 const app = new Application();
 const bot = new TelegramBotWrapper();
-
+function printObject(...obj) {
+  let output = "";
+  for (let i = 0; i < obj.length; i++) {
+    for (let key in obj[i]) {
+      if (typeof obj[i][key] === "object") {
+        output += key + ": { ";
+        for (let innerKey in obj[i][key]) {
+          output += innerKey + ": " + obj[i][key][innerKey] + ", ";
+        }
+        output = output.slice(0, -2);
+        output += " }\n";
+      } else {
+        output += key + ": " + obj[i][key] + "\n";
+      }
+    }
+  }
+  return output;
+}
 // console.log = (...args) => {
 //   bot.sendLog(...args);
 //   originalConsoleLog(...args);
 // };
 console.error = (...args) => {
-  bot.sendError(...args);
-  originalConsoleError(...args);
+  bot.sendError(printObject(...args));
+  originalConsoleError(printObject(...args));
 };
 console.warn = (...args) => {
-  bot.sendWarning(...args);
-  originalConsoleWarn(...args);
+  bot.sendWarning(printObject(...args));
+  originalConsoleWarn(printObject(...args));
 };
 // const cron = require("./resetCounterCronJob");
 
