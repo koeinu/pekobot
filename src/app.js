fa7a65cc903f048aa81a82675024740a12b2b53d
Alexander <aradostev@softaria.com>
2023-04-29 22:49:16 +0900

    wip

--- src/app.js
+++ src/app.js
@@ -7,39 +7,18 @@ import {
 
 const app = new Application();
 const bot = new TelegramBotWrapper();
-function printObject(...obj) {
-  let output = "";
-  for (let i = 0; i < obj.length; i++) {
-    for (let key in obj[i]) {
-      if (typeof obj[i][key] === "object") {
-        output += key + ": { ";
-        for (let innerKey in obj[i][key]) {
-          output +=
-            innerKey +
-            ": " +
-            String(obj[i][key][innerKey]).replace(/\n/g, "\\n") +
-            ", ";
-        }
-        output = output.slice(0, -2);
-        output += " }\n";
-      } else {
-        output += key + ": " + String(obj[i][key]).replace(/\n/g, "\\n") + "\n";
-      }
-    }
-  }
-  return output;
-}
+
 // console.log = (...args) => {
 //   bot.sendLog(...args);
 //   originalConsoleLog(...args);
 // };
 console.error = (...args) => {
-  bot.sendError(printObject(...args));
-  originalConsoleError(printObject(...args));
+  bot.sendError(args.map((arg) => JSON.stringify(arg, null, 4)));
+  originalConsoleError(args.map((arg) => JSON.stringify(arg, null, 4)));
 };
 console.warn = (...args) => {
-  bot.sendWarning(printObject(...args));
-  originalConsoleWarn(printObject(...args));
+  bot.sendWarning(args.map((arg) => JSON.stringify(arg, null, 4)));
+  originalConsoleWarn(args.map((arg) => JSON.stringify(arg, null, 4)));
 };
 // const cron = require("./resetCounterCronJob");
 
