0416dd10b27558307da6de06e0ba7c237aaa858e
Alexander <aradostev@softaria.com>
2023-05-03 08:49:30 +0900

    inactive option

--- src/app.js
+++ src/app.js
@@ -5,25 +5,31 @@ import {
   originalConsoleWarn,
   TelegramBotWrapper,
 } from "./telegramLogger.js";
-
 import cronTask from "./resetCounterCronJob.js";
-console.log("Cron task started:", cronTask);
 
-const app = new Application();
-console.log("Application started:", app);
-const bot = new TelegramBotWrapper();
-console.log("Bot started:", bot);
+import dotenv from "dotenv";
+dotenv.config();
+const inactive = process.env.INACTIVE;
 
-console.log = (...args) => {
-  originalConsoleLog(...args);
-};
-console.error = (...args) => {
-  bot.sendError(...args);
-  originalConsoleError(...args);
-};
-console.warn = (...args) => {
-  bot.sendWarning(...args);
-  originalConsoleWarn(...args);
-};
-console.log("Logging override complete");
-// const testInterval = require("./testLimiterCronJob");
+if (!inactive) {
+  console.log("Cron task started:", cronTask);
+
+  const app = new Application();
+  console.log("Application started:", app);
+  const bot = new TelegramBotWrapper();
+  console.log("Bot started:", bot);
+
+  console.log = (...args) => {
+    originalConsoleLog(...args);
+  };
+  console.error = (...args) => {
+    bot.sendError(...args);
+    originalConsoleError(...args);
+  };
+  console.warn = (...args) => {
+    bot.sendWarning(...args);
+    originalConsoleWarn(...args);
+  };
+  console.log("Logging override complete");
+  // const testInterval = require("./testLimiterCronJob");
+}
