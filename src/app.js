9f3a6f59635eb0e72cdd1181d0bbe3ac0e21cd4a
Alex <aradostev@softaria.com>
2023-05-04 10:42:00 +0900

    logging

--- src/app.js
+++ src/app.js
@@ -31,7 +31,7 @@ if (!inactive) {
     bot.sendWarning(...args);
     originalConsoleWarn(...args);
   };
-  console.log("Logging override complete");
+  console.warn("Logging override complete (a restart happened?)");
 
   const app = new Application();
 
