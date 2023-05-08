8546b531d562ce361877c5da6897f15959d70be6
Alex <aradostev@softaria.com>
2023-05-08 10:13:15 +0900

    asmr calendar

--- src/utils/calendarUtils.js
+++ src/utils/calendarUtils.js
@@ -2,10 +2,18 @@ import dotenv from "dotenv";
 import generateIcs from "ics-service/generate-ics.js";
 import { getYoutubeLiveDetails } from "./youtubeUtils.js";
 import { getCalendarData, updateCalendarData } from "../model/calendars.js";
+import axios from "axios";
+import ical from "ical";
 
 dotenv.config();
 const ICS_DATA = process.env.ICS_DATA;
 const ICS_TIMEOUT = process.env.ICS_TIMEOUT;
+
+const config = {
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
 
 export const CALENDAR_METADATA = ICS_DATA
   ? ICS_DATA.split(";").map((el) => {
@@ -53,6 +61,28 @@ export const prepareCalendarDataFromChannelId = async (
 };
 
 export const getCalendar = async (feedUrl, vtuberHandle, channelId) => {
+  if (vtuberHandle === "asmr") {
+    const data = await axios
+      .get("https://sarisia.cc/holodule-ics/holodule-all.ics", config)
+      .then((resp) => {
+        const cal = resp.data;
+        const data = ical.parseICS(cal);
+        const asmrEvents = Object.entries(data).filter(
+          ([key, calendarData]) => {
+            if (calendarData.summary.toLowerCase().includes("asmr")) {
+              return true;
+            }
+            return false;
+          }
+        );
+        console.debug(`Serving ${data.length} entries for ${vtuberHandle}`);
+        return asmrEvents;
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+    return generateIcs(vtuberHandle, data, feedUrl);
+  }
   let data = await prepareCalendarDataFromChannelId(
     vtuberHandle,
     channelId,
