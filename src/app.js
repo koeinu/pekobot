630f2090060068bd008e2768240c0a8d7d16873a
Alex <aradostev@softaria.com>
2023-05-14 17:15:22 +0900

    inactive mode

--- src/app.js
+++ src/app.js
@@ -17,57 +17,63 @@ import { TwitterClient } from "./twitterClient.js";
 
 dotenv.config();
 
-const bot = new TelegramBotWrapper();
-console.log("Telegram bot started");
+const INACTIVE = process.env.INACTIVE;
 
-console.log = (...args) => {
-  originalConsoleLog(...args);
-};
-console.error = (...args) => {
-  bot.sendError(...args);
-  originalConsoleError(...args);
-};
-console.warn = (...args) => {
-  bot.sendWarning(...args);
-  originalConsoleWarn(...args);
-};
-console.debug = (...args) => {
-  bot.sendDebug(...args);
-  originalConsoleDebug(...args);
-};
-console.error("Logging override complete (a restart happened?)");
+if (!INACTIVE) {
+  const bot = new TelegramBotWrapper();
+  console.log("Telegram bot started");
 
-const expressApp = express();
-const init = async () => {
-  try {
-    const pekoBot = new Application("peko-bot");
-    const mikoBot = new Application("Mikodanye");
+  console.log = (...args) => {
+    originalConsoleLog(...args);
+  };
+  console.error = (...args) => {
+    bot.sendError(...args);
+    originalConsoleError(...args);
+  };
+  console.warn = (...args) => {
+    bot.sendWarning(...args);
+    originalConsoleWarn(...args);
+  };
+  console.debug = (...args) => {
+    bot.sendDebug(...args);
+    originalConsoleDebug(...args);
+  };
+  console.error("Logging override complete (a restart happened?)");
 
-    const twitterClient = new TwitterClient();
-    twitterClient.init([pekoBot, mikoBot]);
-  } catch (e) {
-    console.error(`Couldn't initialize discord bots:`, e);
-  }
-  try {
-    for (let meta of CALENDAR_METADATA) {
-      expressApp.use(
-        "/ics/" + meta.handle + "/feed",
-        feedRoute(async (feedUrl) => {
-          return getCalendar(feedUrl, meta.handle, meta.id);
-        })
-      );
-      expressApp.use(
-        "/ics/" + meta.handle,
-        aboutRoute(meta.handle, "/ics/" + meta.handle + "/feed")
-      );
+  const expressApp = express();
+  const init = async () => {
+    try {
+      const pekoBot = new Application("peko-bot");
+      const mikoBot = new Application("Mikodanye");
+
+      const twitterClient = new TwitterClient();
+      twitterClient.init([pekoBot, mikoBot]);
+    } catch (e) {
+      console.error(`Couldn't initialize discord bots:`, e);
     }
+    try {
+      for (let meta of CALENDAR_METADATA) {
+        expressApp.use(
+          "/ics/" + meta.handle + "/feed",
+          feedRoute(async (feedUrl) => {
+            return getCalendar(feedUrl, meta.handle, meta.id);
+          })
+        );
+        expressApp.use(
+          "/ics/" + meta.handle,
+          aboutRoute(meta.handle, "/ics/" + meta.handle + "/feed")
+        );
+      }
 
-    expressApp.listen(3000);
-  } catch (e) {
-    console.error(`Couldn't create calendar:`, e);
-  }
-};
+      expressApp.listen(3000);
+    } catch (e) {
+      console.error(`Couldn't create calendar:`, e);
+    }
+  };
 
-init().catch((e) => {
-  console.error(`Couldn't initialize application:`, e);
-});
+  init().catch((e) => {
+    console.error(`Couldn't initialize application:`, e);
+  });
+} else {
+  console.log("Inactive mode");
+}
