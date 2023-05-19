638bf80d031395711f4be9c600859578ccb11c56
Alex <aradostev@softaria.com>
2023-05-19 21:33:00 +0900

    who called the commands?

--- src/utils/stringUtils.js
+++ src/utils/stringUtils.js
@@ -163,7 +163,7 @@ export const gatherSlashCommandInfo = (interaction) => {
   if (sc) {
     parts.push(sc);
   }
-  console.log("slash command interaction:", JSON.stringify(interaction));
+  console.log("slash command interaction:", interaction);
   parts.push(`at ${getMsgInfo(interaction)} |`);
   parts.push(...values);
   return parts;
