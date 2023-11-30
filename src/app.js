9689d5e2684c07ded521330aac6c5b0b464df467
Alex <aradostev@softaria.com>
2023-11-30 23:37:53 +0900

    fix

--- src/app.js
+++ src/app.js
@@ -1,3 +1,13 @@
+import { Application } from "./application.js";
+import {
+  originalConsoleDebug,
+  originalConsoleError,
+  originalConsoleLog,
+  originalConsoleWarn,
+  TelegramBotWrapper,
+} from "./telegramLogger.js";
+import { cronTask } from "./resetCounterCronJob.js";
+
 import dotenv from "dotenv";
 import express from "express";
 import aboutRoute from "ics-service/about.js";
@@ -7,6 +17,20 @@ import {
   createCalendarRoute,
   getCalendar,
 } from "./utils/calendarUtils.js";
+import { TwitterClient } from "./twitterClient.js";
+import {
+  createLogsRoute,
+  DEBUGS_FILENAME,
+  ERRORS_FILENAME,
+  LOGS_FILENAME,
+  WARNINGS_FILENAME,
+  writeDebug,
+  writeError,
+  writeLog,
+  writeWarning,
+} from "./model/logs.js";
+import { createUploadSettingsRoute } from "./model/botSettings.js";
+
 dotenv.config();
 
 const INACTIVE = process.env.INACTIVE;
@@ -17,8 +41,55 @@ const IGNORED_WARNINGS = [
 ];
 
 if (!INACTIVE) {
+  const bot = new TelegramBotWrapper();
+  console.log("Telegram bot started");
+
+  console.log = (...args) => {
+    writeLog(...args);
+    originalConsoleLog(...args);
+  };
+  console.error = (...args) => {
+    if (
+      IGNORED_WARNINGS.some((warning) =>
+        args.some((arg) => typeof arg === "string" && arg.includes(warning))
+      )
+    ) {
+      return;
+    }
+    writeError(...args);
+    bot.sendError(...args);
+    originalConsoleError(...args);
+  };
+  console.warn = (...args) => {
+    if (
+      IGNORED_WARNINGS.some((warning) =>
+        args.some((arg) => typeof arg === "string" && arg.includes(warning))
+      )
+    ) {
+      return;
+    }
+    writeWarning(...args);
+    bot.sendWarning(...args);
+    originalConsoleWarn(...args);
+  };
+  console.debug = (...args) => {
+    writeDebug(...args);
+    bot.sendDebug(...args);
+    originalConsoleDebug(...args);
+  };
+  console.error("Logging override complete (a restart happened?)");
+
   const expressApp = express();
   const init = async () => {
+    try {
+      const pekoBot = new Application("peko-bot");
+      const mikoBot = new Application("Mikodanye");
+
+      const twitterClient = new TwitterClient();
+      twitterClient.init([pekoBot, mikoBot]);
+    } catch (e) {
+      console.error(`Couldn't initialize discord bots:`, e);
+    }
     try {
       for (let meta of CALENDAR_METADATA) {
         expressApp.use(
@@ -31,8 +102,14 @@ if (!INACTIVE) {
           "/ics/" + meta.handle,
           aboutRoute(meta.handle, "/ics/" + meta.handle + "/feed")
         );
+        expressApp.use("/cal", createCalendarRoute());
+        expressApp.use("/log", createLogsRoute(LOGS_FILENAME));
+        expressApp.use("/warn", createLogsRoute(WARNINGS_FILENAME));
+        expressApp.use("/error", createLogsRoute(ERRORS_FILENAME));
+        expressApp.use("/debug", createLogsRoute(DEBUGS_FILENAME));
+        expressApp.put("/upload", createUploadSettingsRoute);
+        expressApp.put("/uploadCalendar", createUploadSettingsRoute);
       }
-      expressApp.use("/cal", createCalendarRoute());
 
       expressApp.listen(8166);
     } catch (e) {
