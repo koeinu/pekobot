c5a6d4b8ef613222c1f37c76454c5a3615a32bdd
Alex <aradostev@softaria.com>
2023-12-01 18:37:45 +0900

    some cleaning

--- src/app.js
+++ src/app.js
@@ -1,11 +1,4 @@
 import { Application } from "./application.js";
-import {
-  originalConsoleDebug,
-  originalConsoleError,
-  originalConsoleLog,
-  originalConsoleWarn,
-  TelegramBotWrapper,
-} from "./telegramLogger.js";
 
 import dotenv from "dotenv";
 import express from "express";
@@ -15,52 +8,13 @@ dotenv.config();
 
 const INACTIVE = process.env.INACTIVE;
 
-const IGNORED_WARNINGS = [
-  "ExperimentalWarning: buffer.Blob is an experimental feature",
-  "The Fetch API is an experimental feature",
-];
-
 if (!INACTIVE) {
-  // const bot = new TelegramBotWrapper();
-  // console.log = (...args) => {
-  //   // writeLog(...args);
-  //   originalConsoleLog(...args);
-  // };
-  // console.error = (...args) => {
-  //   if (
-  //     IGNORED_WARNINGS.some((warning) =>
-  //       args.some((arg) => typeof arg === "string" && arg.includes(warning))
-  //     )
-  //   ) {
-  //     return;
-  //   }
-  //   // writeError(...args);
-  //   bot.sendError(...args);
-  //   originalConsoleError(...args);
-  // };
-  // console.warn = (...args) => {
-  //   if (
-  //     IGNORED_WARNINGS.some((warning) =>
-  //       args.some((arg) => typeof arg === "string" && arg.includes(warning))
-  //     )
-  //   ) {
-  //     return;
-  //   }
-  //   // writeWarning(...args);
-  //   bot.sendWarning(...args);
-  //   originalConsoleWarn(...args);
-  // };
-  // console.debug = (...args) => {
-  //   // writeDebug(...args);
-  //   bot.sendDebug(...args);
-  //   originalConsoleDebug(...args);
-  // };
   console.error("Logging override complete (a restart happened?)");
 
   const expressApp = express();
   const init = async () => {
     try {
-      const bot = new Application("jigsaw-bot");
+      new Application("jigsaw-bot");
     } catch (e) {
       console.error(`Couldn't initialize discord bots:`, e);
     }
@@ -68,7 +22,7 @@ if (!INACTIVE) {
       expressApp.put("/upload", createUploadSettingsRoute);
       expressApp.put("/uploadCalendars", createUploadSettingsRoute);
 
-      expressApp.listen(3001);
+      expressApp.listen(3000);
     } catch (e) {
       console.error(`Couldn't create calendar:`, e);
     }
